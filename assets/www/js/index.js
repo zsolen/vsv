5934de74e0436d0a28728cfd1d25dce593d24e2e
zsolen <zsolt.lenkei@triona.se>
2013-07-25 10:40:06 +0200

    debug config

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -1,38 +1,49 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-
-    }
+function init() {
+        document.addEventListener("deviceready", onDeviceReady, false);
 };
+
+function onDeviceReady() {
+
+};
+
+$(document).on('pagebeforeshow', '#login-page', function() {
+    $(document).on('click', '#submitButton', function() {
+        $("#submitButton").attr("disabled","disabled");
+        if($('#username').val().length > 0 && $('#password').val().length > 0){
+            var u = $('#username', form).val();
+            var p = $('#password', form).val();
+            $.ajax({
+                url: 'check.php',
+                data: {action : 'login', formData : $('#loginForm').serialize()}, // Convert a form to a JSON string representation
+                type: 'post',
+                async: true,
+                beforeSend: function() {
+                    $.mobile.showPageLoadingMsg(true);
+                },
+                complete: function() {
+                    $.mobile.hidePageLoadingMsg();
+                },
+                success: function (result) {
+                    if(result == "true"){
+                        window.localStorage["username"] = u;
+                        window.localStorage["password"] = p;
+                        $.mobile.changePage("startpage.html", { transition: "pop" });
+                    }
+                    else{
+                        alert('Fel användarnamn eller lösenord.');
+                    }
+                    $("#submitButton").removeAttr("disabled");
+                },
+                error: function (request,error) {
+                    alert('Fel i ankopling mot servern.');
+                    $("#submitButton").removeAttr("disabled");
+                }
+            });
+        }
+        else {
+            alert('Användarnamn och/eller lösenord saknas')
+            $("#submitButton").removeAttr("disabled");
+        }
+        return false;
+    });
+});
